4c1f970c76d8a30e664175b014cd3e7b9039116c
nthnelliott857 <91151031+nthnelliott857@users.noreply.github.com>
2021-12-26 10:31:26 -0800

    Add files via upload

---
+++ Hill Cipher Project/AnotherTestProgram.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class AnotherTestProgram {
+   public static void main(String[] args) {
+      //a - floor(a / b) * b
+      
+      List<Integer> list = new ArrayList<Integer>();
+      
+      int a = 549 / 163;
+      /*Iint b = 26;
+      
+      double c = a - Math.floor(a / b) * b;
+      
+      System.out.println(c);*/
+   
+   }
+
+}
